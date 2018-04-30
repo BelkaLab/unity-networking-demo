002ad3f740de8440c705cc620a17c4959677b06e
Massimo Frasson <maxfrax@gmail.com>
2018-04-30 16:01:27 +0200

    Player moves and enables only local camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraController : NetworkBehaviour
+{
+
+
+	public new GameObject camera;
+
+	private void Start()
+	{
+		camera.SetActive(isLocalPlayer);
+	}
+}
---
+++ Assets/MovementController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MovementController : NetworkBehaviour
+{
+	public float speed = 2f;
+
+	private void Start()
+	{
+		transform.position = new Vector3(0f, 2f, 0f);
+	}
+
+	private void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += transform.forward * speed;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position -= transform.forward * speed;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position -= transform.right * speed;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += transform.right * speed;
+		}
+	}
+}
