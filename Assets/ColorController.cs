5591571991770ef57060bfdaedad0df0cc07d559
Giovanni Frigo <giovanni@belka.us>
2018-05-02 17:12:58 +0200

    Adding some comments

--- Assets/ColorController.cs
+++ Assets/ColorController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-[RequireComponent(typeof(Renderer))]
 public class ColorController : NetworkBehaviour
 {
     [SyncVar] private Color color = Color.white;
@@ -14,17 +13,23 @@ public class ColorController : NetworkBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isLocalPlayer)
+        if (isLocalPlayer) // allow only ourselves to change the color
         {
-            CmdChangeColor();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // ask the server to change our color
+                CmdChangeColor();
+            }
         }
 
+        // update the color with the value received from the server
         renderer.material.color = color;
     }
 
     [Command]
     private void CmdChangeColor()
     {
+        // change the color in all the clients
         color = new Color(Random.value, Random.value, Random.value, 1f);
     }
 }
